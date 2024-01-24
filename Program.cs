39eb1eedb093ea24e322cc082bc919ecce149b6d
Lenovo-User <Lenovo-User@DESKTOP-QJ3UI1M>
2024-01-24 17:43:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.STATIC_METHODS_AND_CLASSES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+            usefulltools.sayhi("tebogo");
+
+            Console.Read();
+        }
+    }
+}
---
+++ usefulltools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.STATIC_METHODS_AND_CLASSES
+{
+    internal class usefulltools
+    {
+        public static void sayhi(string name)
+        {
+            Console.WriteLine("hi " + name);
+        }
+    }
+}
